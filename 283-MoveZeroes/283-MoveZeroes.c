e65cb4de03755a35214f089d592b64ffb9625bb7
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:21:13 +0530

    File updated at 4/22/2026, 12:21:12 AM

--- 283-MoveZeroes/283-MoveZeroes.c
+++ 283-MoveZeroes/283-MoveZeroes.c
@@ -1,10 +1,17 @@
-// Last updated: 4/22/2026, 12:20:35 AM
-1int missingNumber(int* nums, int numsSize) {
-2    int xor = numsSize;  // include n
-3
-4    for (int i = 0; i < numsSize; i++) {
-5        xor ^= i ^ nums[i];
-6    }
-7
-8    return xor;
-9}
+// Last updated: 4/22/2026, 12:21:12 AM
+1int maxSubArray(int* nums, int numsSize) {
+2    int maxSum = nums[0];
+3    int currentSum = nums[0];
+4
+5    for (int i = 1; i < numsSize; i++) {
+6        if (currentSum < 0)
+7            currentSum = nums[i];
+8        else
+9            currentSum += nums[i];
+10
+11        if (currentSum > maxSum)
+12            maxSum = currentSum;
+13    }
+14
+15    return maxSum;
+16}
