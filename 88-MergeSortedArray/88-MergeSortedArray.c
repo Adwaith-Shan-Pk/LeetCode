a8847280a24cb00acfb71a70c4c83d19b226626a
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:39:32 +0530

    File updated at 4/22/2026, 12:39:31 AM

--- 88-MergeSortedArray/88-MergeSortedArray.c
+++ 88-MergeSortedArray/88-MergeSortedArray.c
@@ -1,60 +1,23 @@
-// Last updated: 4/22/2026, 12:35:28 AM
-1#include <stdlib.h>
-2
-3int cmp(const void* a, const void* b) {
-4    return (*(int*)a - *(int*)b);
-5}
-6
-7// helper: number of starts <= x
-8int countStarts(int* arr, int n, int x) {
-9    int l = 0, r = n;
-10    while (l < r) {
-11        int m = l + (r - l) / 2;
-12        if (arr[m] <= x) l = m + 1;
-13        else r = m;
-14    }
-15    return l;
-16}
-17
-18// helper: number of ends < x
-19int countEnds(int* arr, int n, int x) {
-20    int l = 0, r = n;
-21    while (l < r) {
-22        int m = l + (r - l) / 2;
-23        if (arr[m] < x) l = m + 1;
-24        else r = m;
-25    }
-26    return l;
-27}
-28
-29int* fullBloomFlowers(int** flowers, int flowersSize, int* flowersColSize,
-30                      int* people, int peopleSize, int* returnSize) {
-31
-32    int* starts = (int*)malloc(flowersSize * sizeof(int));
-33    int* ends   = (int*)malloc(flowersSize * sizeof(int));
-34
-35    for (int i = 0; i < flowersSize; i++) {
-36        starts[i] = flowers[i][0];
-37        ends[i] = flowers[i][1];
-38    }
-39
-40    qsort(starts, flowersSize, sizeof(int), cmp);
-41    qsort(ends, flowersSize, sizeof(int), cmp);
-42
-43    int* ans = (int*)malloc(peopleSize * sizeof(int));
-44
-45    for (int i = 0; i < peopleSize; i++) {
-46        int p = people[i];
-47
-48        int started = countStarts(starts, flowersSize, p);
-49        int ended = countEnds(ends, flowersSize, p);
-50
-51        ans[i] = started - ended;
-52    }
-53
-54    free(starts);
-55    free(ends);
-56
-57    *returnSize = peopleSize;
-58    return ans;
-59}
+// Last updated: 4/22/2026, 12:39:31 AM
+1#include <stdio.h>
+2#include <stdlib.h>
+3
+4int cmp(const void *a, const void *b) {
+5    return (*(int*)a - *(int*)b);
+6}
+7
+8int largestPerimeter(int* nums, int numsSize) {
+9    qsort(nums, numsSize, sizeof(int), cmp);
+10
+11    for (int i = numsSize - 3; i >= 0; i--) {
+12        long long a = nums[i];
+13        long long b = nums[i + 1];
+14        long long c = nums[i + 2];
+15
+16        if (a + b > c) {
+17            return (int)(a + b + c);
+18        }
+19    }
+20
+21    return 0;
+22}
