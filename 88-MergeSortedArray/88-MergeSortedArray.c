c52ac402309c8bc9a82bc0353b20f58fbca6b2e7
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:31:49 +0530

    File updated at 4/22/2026, 12:31:47 AM

--- 88-MergeSortedArray/88-MergeSortedArray.c
+++ 88-MergeSortedArray/88-MergeSortedArray.c
@@ -1,19 +1,17 @@
-// Last updated: 4/22/2026, 12:30:43 AM
-1void merge(int* nums1, int nums1Size, int m,
-2           int* nums2, int nums2Size, int n) {
-3    
-4    int i = m - 1;
-5    int j = n - 1;
-6    int k = m + n - 1;
-7
-8    while (i >= 0 && j >= 0) {
-9        if (nums1[i] > nums2[j])
-10            nums1[k--] = nums1[i--];
-11        else
-12            nums1[k--] = nums2[j--];
-13    }
-14
-15    while (j >= 0) {
-16        nums1[k--] = nums2[j--];
-17    }
-18}
+// Last updated: 4/22/2026, 12:31:47 AM
+1#include <stdlib.h>
+2
+3int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
+4    int* result = (int*)malloc(numsSize * sizeof(int));
+5    int left = 0, right = numsSize - 1;
+6
+7    for (int i = 0; i < numsSize; i++) {
+8        if (nums[i] % 2 == 0)
+9            result[left++] = nums[i];
+10        else
+11            result[right--] = nums[i];
+12    }
+13
+14    *returnSize = numsSize;
+15    return result;
+16}
