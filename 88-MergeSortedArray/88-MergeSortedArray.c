dc82df7958047bb7880ea7cd08d58f1d9e7fbdfa
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:41:58 +0530

    File updated at 4/22/2026, 12:41:57 AM

--- 88-MergeSortedArray/88-MergeSortedArray.c
+++ 88-MergeSortedArray/88-MergeSortedArray.c
@@ -1,16 +1,14 @@
-// Last updated: 4/22/2026, 12:40:59 AM
+// Last updated: 4/22/2026, 12:41:57 AM
 1#include <stdio.h>
 2
-3int fib(int n) {
-4    if (n <= 1) return n;
+3int minCostClimbingStairs(int* cost, int costSize) {
+4    int a = cost[0], b = cost[1], c;
 5
-6    int a = 0, b = 1, c;
-7
-8    for (int i = 2; i <= n; i++) {
-9        c = a + b;
-10        a = b;
-11        b = c;
-12    }
-13
-14    return b;
-15}
+6    for (int i = 2; i < costSize; i++) {
+7        c = cost[i] + (a < b ? a : b);
+8        a = b;
+9        b = c;
+10    }
+11
+12    return (a < b ? a : b);
+13}
