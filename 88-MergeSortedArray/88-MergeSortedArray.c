f12ed26519f557a063d761f0e3aacc11822d6009
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:41:00 +0530

    File updated at 4/22/2026, 12:40:59 AM

--- 88-MergeSortedArray/88-MergeSortedArray.c
+++ 88-MergeSortedArray/88-MergeSortedArray.c
@@ -1,12 +1,12 @@
-// Last updated: 4/22/2026, 12:40:23 AM
+// Last updated: 4/22/2026, 12:40:59 AM
 1#include <stdio.h>
 2
-3int climbStairs(int n) {
-4    if (n <= 2) return n;
+3int fib(int n) {
+4    if (n <= 1) return n;
 5
-6    int a = 1, b = 2, c;
+6    int a = 0, b = 1, c;
 7
-8    for (int i = 3; i <= n; i++) {
+8    for (int i = 2; i <= n; i++) {
 9        c = a + b;
 10        a = b;
 11        b = c;
