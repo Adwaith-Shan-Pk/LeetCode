7c4659f42c3b970d28f12d3ecf9d29c6e6be8d65
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:19:41 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.c
@@ -0,0 +1,18 @@
+// Last updated: 4/22/2026, 12:19:40 AM
+1void moveZeroes(int* nums, int numsSize) {
+2    int j = 0;  // position for next non-zero
+3    
+4    // Move all non-zero elements forward
+5    for (int i = 0; i < numsSize; i++) {
+6        if (nums[i] != 0) {
+7            nums[j] = nums[i];
+8            j++;
+9        }
+10    }
+11    
+12    // Fill remaining positions with zeros
+13    while (j < numsSize) {
+14        nums[j] = 0;
+15        j++;
+16    }
+17}
