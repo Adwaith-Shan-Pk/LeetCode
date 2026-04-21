85b65b44cdef5ec3f766c1f0f6787b38ed217fed
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:23:00 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.c
@@ -0,0 +1,17 @@
+// Last updated: 4/22/2026, 12:22:59 AM
+int maxSubArray(int* nums, int numsSize) {
+    int maxSum = nums[0];
+    int currentSum = nums[0];
+
+    for (int i = 1; i < numsSize; i++) {
+        if (currentSum < 0)
+            currentSum = nums[i];
+        else
+            currentSum += nums[i];
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    return maxSum;
+}
