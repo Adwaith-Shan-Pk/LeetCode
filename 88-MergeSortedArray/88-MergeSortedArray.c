2528c1fb50f0c1a048b2460560f68d28f743218e
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:32:30 +0530

    File updated at 4/22/2026, 12:32:28 AM

--- 88-MergeSortedArray/88-MergeSortedArray.c
+++ 88-MergeSortedArray/88-MergeSortedArray.c
@@ -1,17 +1,22 @@
-// Last updated: 4/22/2026, 12:31:47 AM
+// Last updated: 4/22/2026, 12:32:28 AM
 1#include <stdlib.h>
 2
-3int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
-4    int* result = (int*)malloc(numsSize * sizeof(int));
-5    int left = 0, right = numsSize - 1;
+3int cmp(const void* a, const void* b) {
+4    return (*(int*)a - *(int*)b);
+5}
 6
-7    for (int i = 0; i < numsSize; i++) {
-8        if (nums[i] % 2 == 0)
-9            result[left++] = nums[i];
-10        else
-11            result[right--] = nums[i];
-12    }
-13
-14    *returnSize = numsSize;
-15    return result;
-16}
+7int findContentChildren(int* g, int gSize, int* s, int sSize) {
+8    qsort(g, gSize, sizeof(int), cmp);
+9    qsort(s, sSize, sizeof(int), cmp);
+10
+11    int i = 0, j = 0;
+12
+13    while (i < gSize && j < sSize) {
+14        if (s[j] >= g[i]) {
+15            i++;  // child satisfied
+16        }
+17        j++;      // move to next cookie always
+18    }
+19
+20    return i;
+21}
