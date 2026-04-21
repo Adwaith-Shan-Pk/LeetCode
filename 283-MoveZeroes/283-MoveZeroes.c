49be05111062e753e67fda10b52ba39db96957b9
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-04-22 00:20:36 +0530

    File updated at 4/22/2026, 12:20:35 AM

--- 283-MoveZeroes/283-MoveZeroes.c
+++ 283-MoveZeroes/283-MoveZeroes.c
@@ -1,18 +1,10 @@
-// Last updated: 4/22/2026, 12:19:51 AM
-1void moveZeroes(int* nums, int numsSize) {
-2    int j = 0;  // position for next non-zero
-3    
-4    // Move all non-zero elements forward
-5    for (int i = 0; i < numsSize; i++) {
-6        if (nums[i] != 0) {
-7            nums[j] = nums[i];
-8            j++;
-9        }
-10    }
-11    
-12    // Fill remaining positions with zeros
-13    while (j < numsSize) {
-14        nums[j] = 0;
-15        j++;
-16    }
-17}
+// Last updated: 4/22/2026, 12:20:35 AM
+1int missingNumber(int* nums, int numsSize) {
+2    int xor = numsSize;  // include n
+3
+4    for (int i = 0; i < numsSize; i++) {
+5        xor ^= i ^ nums[i];
+6    }
+7
+8    return xor;
+9}
