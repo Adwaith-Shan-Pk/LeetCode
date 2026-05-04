3bbdb065497285e339dd4be3c4d77a4c001ee626
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-05-04 23:48:42 +0530

    File updated at 5/4/2026, 11:48:42 PM

--- 77-Combinations/77-Combinations.py
+++ 77-Combinations/77-Combinations.py
@@ -1,17 +1,31 @@
-# Last updated: 5/4/2026, 11:43:26 PM
+# Last updated: 5/4/2026, 11:48:42 PM
 1class Solution:
-2    def combine(self, n, k):
-3        res = []
-4        
-5        def backtrack(start, path):
-6            if len(path) == k:
-7                res.append(path[:])
-8                return
-9            
-10            for i in range(start, n + 1):
-11                path.append(i)
-12                backtrack(i + 1, path)
-13                path.pop()
-14        
-15        backtrack(1, [])
-16        return res
+2    def permuteUnique(self, nums):
+3        nums.sort()
+4        res = []
+5        used = [False] * len(nums)
+6        
+7        def backtrack(path):
+8            if len(path) == len(nums):
+9                res.append(path[:])
+10                return
+11            
+12            for i in range(len(nums)):
+13                
+14                if used[i]:
+15                    continue
+16                
+17                
+18                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
+19                    continue
+20                
+21                used[i] = True
+22                path.append(nums[i])
+23                
+24                backtrack(path)
+25                
+26                path.pop()
+27                used[i] = False
+28        
+29        backtrack([])
+30        return res
