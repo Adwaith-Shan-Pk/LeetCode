57511599cc8a2d22dcf58b4fb9a86bf40fadfcfc
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-05-04 23:42:40 +0530

    Create 77-Combinations

---
+++ 77-Combinations/77-Combinations.py
@@ -0,0 +1,17 @@
+# Last updated: 5/4/2026, 11:42:40 PM
+1class Solution:
+2    def combine(self, n, k):
+3        res = []
+4        
+5        def backtrack(start, path):
+6            if len(path) == k:
+7                res.append(path[:])
+8                return
+9            
+10            for i in range(start, n + 1):
+11                path.append(i)
+12                backtrack(i + 1, path)
+13                path.pop()
+14        
+15        backtrack(1, [])
+16        return res
