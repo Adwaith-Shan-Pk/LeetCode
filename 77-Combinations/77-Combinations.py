ef43c0a26a06ddfcd74ad615af87b399dbff377e
Adwaith Shan <89580585+Adwaith-Shan-Pk@users.noreply.github.com>
2026-05-04 23:49:09 +0530

    File updated at 5/4/2026, 11:49:09 PM

--- 77-Combinations/77-Combinations.py
+++ 77-Combinations/77-Combinations.py
@@ -1,31 +1,40 @@
-# Last updated: 5/4/2026, 11:48:42 PM
+# Last updated: 5/4/2026, 11:49:09 PM
 1class Solution:
-2    def permuteUnique(self, nums):
-3        nums.sort()
-4        res = []
-5        used = [False] * len(nums)
-6        
-7        def backtrack(path):
-8            if len(path) == len(nums):
-9                res.append(path[:])
-10                return
-11            
-12            for i in range(len(nums)):
-13                
-14                if used[i]:
-15                    continue
-16                
-17                
-18                if i > 0 and nums[i] == nums[i-1] and not used[i-1]:
-19                    continue
-20                
-21                used[i] = True
-22                path.append(nums[i])
-23                
-24                backtrack(path)
-25                
-26                path.pop()
-27                used[i] = False
-28        
-29        backtrack([])
-30        return res
+2    def removeInvalidParentheses(self, s):
+3        from collections import deque
+4
+5        def is_valid(st):
+6            bal = 0
+7            for ch in st:
+8                if ch == '(':
+9                    bal += 1
+10                elif ch == ')':
+11                    bal -= 1
+12                    if bal < 0:
+13                        return False
+14            return bal == 0
+15
+16        res = []
+17        visited = set([s])
+18        q = deque([s])
+19        found = False
+20
+21        while q:
+22            cur = q.popleft()
+23
+24            if is_valid(cur):
+25                res.append(cur)
+26                found = True
+27
+28            if found:
+29                continue
+30
+31            for i in range(len(cur)):
+32                if cur[i] not in '()':
+33                    continue
+34                nxt = cur[:i] + cur[i+1:]
+35                if nxt not in visited:
+36                    visited.add(nxt)
+37                    q.append(nxt)
+38
+39        return res
